c3938a225d5d2db5f16ebc65c1811ec83a2b22ee
Cannab1z <kidron64@gmail.com>
2021-03-30 00:45:33 +0300

    Added login page, started sessions

---
+++ Yoav/Yoav/Login.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Yoav.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+    <table>
+        <tr>
+            <td><asp:Label ID="User_Name" runat="server" Text="Username"></asp:Label></td>
+            <td><asp:TextBox ID="Username" runat="server" ></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="Password_label" runat="server" Text="Password"></asp:Label></td>
+            <td><asp:TextBox ID="user_pass" TextMode="Password" runat="server" ></asp:TextBox></td>
+        </tr>
+         <tr>
+            <td><asp:Button ID="CheckLogin" runat="server" Text="Login" OnClick="Check_login" /></td>
+             <td><input type="reset" /></td>
+        </tr>
+        
+    </table>
+    <asp:Label ID="label_error" runat="server"></asp:Label>
+    </div>  
+</asp:Content>
---
+++ Yoav/Yoav/Login.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+
+namespace Yoav
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            label_error.Text = "";
+        }
+        protected void Check_login(object sender, EventArgs e)
+        {
+            OleDbConnection con1 = new OleDbConnection();
+            con1.ConnectionString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + Request.PhysicalApplicationPath + "\\Yoav_DB.accdb";
+            con1.Open();
+            string sqlstring = @"SELECT UserName, user_Password FROM users_tbl WHERE UserName = @usr AND user_Password = @pass";
+            OleDbCommand conSer = new OleDbCommand(sqlstring, con1);
+            conSer.Parameters.AddWithValue("@usr", Username.Text);
+            conSer.Parameters.AddWithValue("@pass", user_pass.Text);
+            OleDbDataReader Drdr = conSer.ExecuteReader();
+            Drdr.Read();
+            if (Drdr.HasRows)
+            {
+                Session["user"] = Username.Text;
+                Response.Redirect("ShowUsers.aspx");
+            }
+            else
+            {
+                label_error.Text = "Username and/or password are incorrect";
+            }
+            con1.Close();
+        }
+    }
+}
